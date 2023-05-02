bbce3d669cbabca15cd81c67482803bf61d9841a
Prem <prem86846@gmail.com>
2023-05-02 13:16:04 +0530

    newfile

---
+++ file9.js
@@ -0,0 +1,18 @@
+//String Concatenation (Adding Method)
+
+let string1="Prem";
+let string2="Kumar";
+
+//let fullName=string1+string2;
+let fullName=string1+" "+string2;
+console.log(fullName);
+
+
+//String Concatenation (Number Method)
+
+let string3="17"
+let string4="10"
+//let newstring=string3+string4;
+
+let newstring=+string3 + +string4;
+console.log(newstring);
