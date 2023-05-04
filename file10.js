6f0988a5124b8745e3af44417e1bb659138115a5
Prem <prem86846@gmail.com>
2023-05-04 10:55:11 +0530

    fileadd

---
+++ file10.js
@@ -0,0 +1,12 @@
+//template string
+
+let age =23;
+let firstName="prem";
+
+
+//"my name is prem and my age is 23"
+
+//let aboutMe ="my name is " + firstName + " and my age is "+age;
+
+let aboutMe =`my name is ${firstName} and my age is ${age}`
+console.log(aboutMe);
---
+++ file11.js
@@ -0,0 +1,5 @@
+//undefine
+let firstName;
+console.log(typeof firstName);
+firstName="Harshit";
+console.log(typeof firstName,firstName);
